830304d6d48ce8202a5c925d639a86d1c1fc2d82
exit <nguyenducdaitoan>
2016-12-11 20:54:13 +0700

    init code

--- app/models/amazon_proxy.rb
+++ app/models/amazon_proxy.rb
@@ -4,7 +4,7 @@ class AmazonProxy
   Amazon::Ecs.configure do |options|
     options[:AWS_access_key_id] = ''
     options[:AWS_secret_key]    = ''
-    options[:associate_tag]     = 'YOUR ASSOCIATE TAG'
+    options[:associate_tag]     = 'cheryltee69-20'
   end
 
   private
@@ -20,16 +20,17 @@ class AmazonProxy
 
   def self.save_book(asin, item)
     return if Book.exists?(asin: asin)
-
-    options                    = {}
-    image_url                  = item.get_hash('MediumImage')['URL']
-    item_attributes            = item.get_element('ItemAttributes')
-    options[:asin]             = asin
-    options[:title]            = item_attributes.get('Title')
-    options[:published_at]     = item_attributes.get('PublicationDate')
-    options[:author]           = item_attributes.get_array('Author').join(', ')
-    options[:amazon_image_url] = image_url
-    options[:image_url]        = save_image(asin, image_url)
+    options                       = {}
+    image_url                     = item.get_hash('MediumImage')['URL']
+    item_attributes               = item.get_element('ItemAttributes')
+    options[:asin]                = asin
+    options[:title]               = item_attributes.get('Title')
+    options[:published_at]        = item_attributes.get('PublicationDate')
+    options[:author]              = item_attributes.get_array('Author').join(', ')
+    options[:amazon_image_url]    = image_url
+    options[:image_url]           = save_image(asin, image_url)
+    sales_rank                    = item.get_element('SalesRank')
+    options[:previous_sales_rank] = sales_rank
     Book.create! options
   end
 
